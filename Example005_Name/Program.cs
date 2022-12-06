1e932de53b57546f635ec73bd1a1ae22673d6491
ZaurKhakurinov <lazter@mail.ru>
2022-12-07 00:30:38 +0300

    add Example 005 Name Don't work!!!

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Сonsole.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
